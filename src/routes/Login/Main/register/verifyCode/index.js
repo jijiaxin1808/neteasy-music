433b0127d4da83f868c7c34c2d5ddadc4c22cb4e
jijiaxin1808 <44405174+jijiaxin1808@users.noreply.github.com>
2019-09-15 16:26:40 +0800

    login
    这次登录真的快写完了

--- src/routes/Login/Main/register/verifyCode/index.js
+++ src/routes/Login/Main/register/verifyCode/index.js
@@ -3,6 +3,7 @@ import Button from "../../../../components/Button";
 import Input from "../../../../components/Input";
 import LoginBar from "../../../../components/loginBar";
 import "./index.less";
+import { connect } from "dva/router";
 
 class VerifyCode extends React.Component {
     
@@ -13,7 +14,7 @@ class VerifyCode extends React.Component {
 				<p>您的手机号  +86 {login.RegName}</p>
 
 
-                <span > 重新发送</span>
+				<span > 重新发送</span>
 				<Button type = "blue" content = {"下一步"} />
                 
 			</div>
@@ -25,6 +26,11 @@ class VerifyCode extends React.Component {
 
 
 }
+const mapDispatchToProps = ()=> ({
+
+
+
+});
 
 
 export default connect(({ login })=>({ login }),mapDispatchToProps)(VerifyCode);
