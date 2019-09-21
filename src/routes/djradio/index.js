44dbb074251df57f5d42df2d769885fbe4e1c7db
jijiaxin1808 <44405174+jijiaxin1808@users.noreply.github.com>
2019-09-21 12:18:29 +0800

    修正上次合并的错误

---
+++ src/routes/djradio/index.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import  DjradioItem from "./djradioItem";
+
+class Djradio extends React.Component {
+	render() {
+		return (
+			<Fragment>
+				<DjradioItem />
+			</Fragment>
+		);
+
+
+	}
+
+
+}
+export default Djradio;
