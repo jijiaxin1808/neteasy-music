70c3384dde7774876230a15fa420fea486f83bce
thinkerHope <dannygxm@qq.com>
2019-09-23 19:09:38 +0800

    try playBar ing start

---
+++ src/components/playBar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { connect } from "dva";
+import classnames from "classnames";
+import "./index.less";
+
+class PlayBar extends React.Component {
+
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
+
+export default connect()(PlayBar);
