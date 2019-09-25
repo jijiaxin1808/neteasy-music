1b47b78dbf0a84a19709388a4e6a7ce4d12c141c
thinkerHope <dannygxm@qq.com>
2019-09-25 16:48:17 +0800

    jiaohu

--- src/routes/playBar/audio/index.js
+++ src/routes/playBar/audio/index.js
@@ -4,11 +4,10 @@ const Audio = React.forwardRef((props, ref) => {
 
 	return (
 		<audio
-			src="https://music.163.com/song/media/outer/url?id=103603.mp3"
+			src="https://music.163.com/song/media/outer/url?id=1387152054.mp3"
 			ref={ref}
 			onCanPlay={props.handleCanPlay}
 			onTimeUpdate={props.handleTimeUpdate}
-
 		>
             Your browser does not support the <code>audio</code> element.
 		</audio>
